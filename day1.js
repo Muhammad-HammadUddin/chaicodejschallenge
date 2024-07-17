40d0e0710b580e92a36349341431f4cda99d8f03
Muhammad-HammadUddin <hammaduddin083@gmail.com>
2024-07-17 22:48:02 +0500

    second commit

---
+++ day1.js
@@ -0,0 +1,29 @@
+// Task1
+
+var a=9;
+console.log(a);
+// Task2
+let myName="Hammad Uddin";
+console.log(myName);
+// Task3
+var isLoggedin=true;
+console.log(isLoggedin);
+
+// Task4
+var a=10;
+let brotherName="Owais";
+let student={
+    name:"Hammad Uddin",
+    age:20
+}
+const arr=[1,2,3,4];
+console.log(typeof(a));
+console.log(typeof(brotherName))
+console.log(typeof(student));
+console.log(typeof(arr));
+// Task 5
+myName="hamm";
+console.log(myName);
+// Task6
+arr=[10,20,30];
+console.log(arr);
---
+++ day2.js
@@ -0,0 +1,105 @@
+// Task 1
+let a=10;
+let b=2;
+console.log(a+b);
+// Task 2
+ console.log(a-b);
+ // Task 3
+ console.log(a*b);
+ // Task 4
+ console.log(a/b);
+ // Task 5
+ console.log(a%b);
+ // Task 6
+ let c=11;
+ let d=4;
+ c+=d;
+ console.log(c);
+ // Task 7
+ let e=11;
+ let f=4;
+ e-=f;
+ console.log(e);
+ // Task 8
+ if(e>f){
+    console.log(e);
+    } 
+    else {
+        console.log(f);
+    }
+    // Task 9
+    let g=11;
+    let h=11;
+    if(g>=h){
+        console.log(g);
+    }
+    else{
+        console.log(h);
+    }
+    
+    let i=11;
+    let j=9;
+    if(j<=i){
+        console.log(j);
+    }
+    else{
+        console.log(i);
+    }
+// Task10
+let k=11;
+let l=9;
+if(k==l){
+    console.log("equal");
+}
+else{
+    console.log("not equal");
+}
+
+// Task 11
+if(a>b && j<=i){
+    console.log("Fulfilled");
+}
+else{
+    console.log("Not Fulfilled");
+}
+// Task 12
+if(a>b || j<=i){
+    console.log("Fulfilled");
+}
+else{
+    console.log("Not Fulfilled");
+}
+// Task 13
+if(!(a>b && j<=i)){
+    console.log("Fulfilled");
+}
+else{
+    console.log("Not Fulfilled");
+}
+// Task 14
+let z=10;
+z>0?console.log("greater"):console.log("lesser");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
